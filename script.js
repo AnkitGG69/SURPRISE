6c74d5698603a0baca5fa2e3f4edfc685b729f44
AnkitGG69 <ankitg2031@gmail.com>
2026-01-05 17:44:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const tapText = document.getElementById("tapText");
+const message = document.getElementById("message");
+const hint = document.getElementById("hint");
+const cake = document.getElementById("cake");
+const balloons = document.getElementById("balloons");
+const endText = document.getElementById("endText");
+const music = document.getElementById("birthday-music");
+
+setTimeout(() => {
+  tapText.style.opacity = "1";
+  tapText.style.transform = "translateY(0)";
+}, 1000);
+
+tapText.addEventListener("click", () => {
+  tapText.style.display = "none";
+  message.style.display = "block";
+
+setTimeout(() => {
+  hint.style.display = "block";
+}, 3000);
+});
+
+
+hint.addEventListener("click", () => {
+  message.style.display = "none";
+  hint.style.display = "none";
+  cake.style.display = "block";
+  document.getElementById("cakeHint").style.display = "block";
+});
+
+cake.addEventListener("click", () => {
+  music.play();
+  cake.style.transform = "scale(1.2)";
+  balloons.style.display = "block";
+  balloons.classList.add("balloon-fly");
+  document.getElementById("cakeHint").style.display = "none";	
+  setTimeout(() => {
+    endText.style.display = "block";
+  }, 2000);
+  
+});
